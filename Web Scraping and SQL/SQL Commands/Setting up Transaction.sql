31e897cc17a3502e188fd76a162805a74dad5174
Zavada Sofiia <zavada.sofia@gmail.com>
2026-05-06 16:05:12 +0300

    Update Setting up Transaction.sql

--- Web Scraping and SQL/SQL Commands/Setting up Transaction.sql
+++ Web Scraping and SQL/SQL Commands/Setting up Transaction.sql
@@ -1,1 +1,18 @@
+select * from [Restaurant_DB].[dbo].[geographic]
 
+BEGIN TRANSACTION
+    insert into [Restaurant_DB].[dbo].[geographic] values ('roha', 'maximoff county', 'sing valley');
+    -- Creating a savepoint fro insert
+    SAVE TRANSACTION T1;
+  
+    --
+    SELECT * FROM [Restaurant_DB].[dbo].[geographic]
+    update [Restaurant_DB].[dbo].[geographic] set county = 'contra costa county' where city = 'roha');
+
+    ROLLBACK TRANSACTION T1
+    -- View the table to confirm rollback
+    SELECT * FROM [Restaurant_DB].[dbo].[geographic]
+
+COMMIT
+
+delete from [Restaurant_DB].[dbo].[geographic] where city IN ('chennai', 'roha');
